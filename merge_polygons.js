013e271efee95093bebde2ea8b27fe7551b16a39
Eran Ben Ami <eran.ben-ami@mobileye.com>
2020-07-15 11:16:35 +0300

    new polygons and merge tool

---
+++ merge_polygons.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+function parseGeojson(fn){
+   let info = path.parse(fn);
+   if(info.ext !== '.geojson'){
+      console.log(`${fn} not a geojson. skipping.`);
+      return;
+   }
+   let data = fs.readFileSync(fn);
+   let polygon = JSON.parse(data).features[0];
+   polygon['id'] = info.name;
+    
+   return polygon;
+}
+
+
+function main(inDir){
+   let dir = fs.readdirSync(inDir);
+   
+   let polygons = {type: "FeatureCollection"};
+   polygons['features'] = dir.map(fn => parseGeojson(fn));
+
+   console.log(JSON.stringify(polygons, null, 2));
+}
+
+let inDir = process.argv[2];
+
+main(inDir)
