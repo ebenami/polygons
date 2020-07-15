cc7da8a790376f59abea1ca64551d34e8450870c
Eran Ben Ami <eran.ben-ami@mobileye.com>
2020-07-15 12:38:50 +0300

    merge_polygons :: only handle geojson files in path

--- merge_polygons.js
+++ merge_polygons.js
@@ -4,22 +4,23 @@ const path = require('path');
 function parseGeojson(fn){
    let info = path.parse(fn);
    if(info.ext !== '.geojson'){
-      // console.log(`${fn} not a geojson. skipping.`);
+      console.error(`${fn} not a geojson. skipping.`);
       return;
    }
    let data = fs.readFileSync(fn);
    let polygon = JSON.parse(data).features[0];
    polygon['id'] = info.name;
-    
+   console.error(`parsing ${fn} ${polygon.constructor = Object}`);
    return polygon;
 }
 
 
 function main(inDir){
-   let dir = fs.readdirSync(inDir);
-   
+   let dir = fs.readdirSync(inDir).filter(f => path.extname(f) === '.geojson');
+
+
    let polygons = {type: "FeatureCollection"};
-   polygons['features'] = dir.map(fn => parseGeojson(fn));
+   polygons['features'] = dir.map(fn => parseGeojson(path.join(inDir, fn)));
 
    console.log(JSON.stringify(polygons, null, 2));
 }
